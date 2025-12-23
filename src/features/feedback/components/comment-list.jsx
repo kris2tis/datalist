95ff305d3907595c0f052c8efc3829918fc82561
kris2tis <m.rezaei.coder@gmail.com>
2025-12-24 02:01:34 +0330

    some change

--- src/features/feedback/components/comment-list.jsx
+++ src/features/feedback/components/comment-list.jsx
@@ -2,7 +2,7 @@ import { http } from "../../../httpServices";
 
 export default async function CommentList() {
   const feedback = await http.get("/user/feedback").then(({ data }) => data);
-  const { data } = feedback || [];
+  const data  = feedback?.date || [];
   return (
     <section className="bg-slate-50 dark:bg-slate-800/30 py-12 px-4 border-y border-gray-100 dark:border-gray-700/50">
       <div className="text-center mb-8">
