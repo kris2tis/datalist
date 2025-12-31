4296afc84c3a2f535a4ed2d47982d069078a24c5
kris2tis <m.rezaei.coder@gmail.com>
2025-12-31 09:57:04 +0330

    add drop down component

---
+++ src/shared/components/ui/drop-down.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+import { ArrowIcon } from "../../../shared/assets/icons/icons";
+
+export default function DropDown({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div class="p-5">
+      <div
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="flex items-center justify-between cursor-pointer"
+      >
+        <h4 class="font-bold text-sm ">{title}</h4>
+        <div
+          className={`${
+            isOpen ? "rotate-270" : "rotate-90"
+          } transition-all duration-300`}
+        >
+          <ArrowIcon className="bg-black" />
+        </div>
+      </div>
+
+      {isOpen && <div className="mt-4">{children}</div>}
+    </div>
+  );
+}
